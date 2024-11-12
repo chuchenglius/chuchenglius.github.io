6174ff0913a425cc0012426d79de7f9489d0d092
chuchenglius <chucheng.lee.us@outlook.com>
2024-11-12 14:23:23 -0800

    todoB

--- CNIT133A/Homework5/todo/src/App.jsx
+++ CNIT133A/Homework5/todo/src/App.jsx
@@ -1,3 +1,4 @@
+import Form from "./components/Form";
 import Todo from "./components/Todo";
 
 function App(props) {
@@ -7,23 +8,7 @@ function App(props) {
   return (
     <div className="todoapp stack-large">
       <h1>Todo App</h1>
-      <form>
-        <h2 className="label-wrapper">
-          <label htmlFor="new-todo-input" className="label__lg">
-            What needs to be done?
-          </label>
-        </h2>
-        <input
-          type="text"
-          id="new-todo-input"
-          className="input input__lg"
-          name="text"
-          autoComplete="off"
-        />
-        <button type="submit" className="btn btn__primary btn__lg">
-          Add
-        </button>
-      </form>
+      <Form />
       <h2 id="list-heading">3 tasks remaining</h2>
       <ul
         role="list"
