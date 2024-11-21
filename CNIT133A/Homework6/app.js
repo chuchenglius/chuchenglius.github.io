23386e76d4d5bea5bb1f16a25a083042e066c8d5
chuchenglius <chucheng.lee.us@outlook.com>
2024-11-21 14:26:24 -0800

    Create Homework6 app1

---
+++ CNIT133A/Homework6/app.js
@@ -0,0 +1,10 @@
+const app = Vue.createApp({
+    data() {
+        return {
+        title: 'Getting to know Vue.js',
+        author: 'A student',
+        qty: 1
+        }
+        }
+    })
+app.mount('#app')
